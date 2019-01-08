6b9102c997a5780f836d2c9695bc3b3e5ec0ff38
Dylan Theno <djtheno@gmail.com>
2019-01-07 18:02:22 -0600

    push1

---
+++ assets/javascript/gif.js
@@ -0,0 +1,84 @@
+//array of topics
+
+var queryURL = " https://api.giphy.com/v1/gifs/search?api_key=&q=cat&limit=10&offset=0&rating=G&lang=en";
+var topics = ["batman", "superman", "the flash", "nightwing", "iron man", "spiderman", "hawkeye", "deadpool", "aquaman"];
+
+function giftastic() {
+
+    // creates buttons for each hero
+    function buttons() {
+        $("buttonView").empty();
+        //for loop for array
+        for (var i = 0; i < topics.length; i++) {
+            // makes buttons for array
+            var btn = $("<button>");
+            btn.addClass("super");
+            btn.attr("data-name", topics[i]);
+            btn.text(topics[i]);
+            //append button to html page
+            $("#herobutton").append(a);
+        }
+
+    }
+        //creates onclick function for button
+        $("newHero").on("click", function () {
+
+            var good = $("#heros").val().trim();
+            topics.push(good);
+            makeButtons();
+            return false;
+
+
+        });
+
+        // function tha displays the gifs
+        function displayGifs() {
+            var good = $(this).attr("data-name");
+            var queryUrl = "var queryURL = "";
+
+            //calls to ajax
+            $.ajax({ url: queryURL, method: "GET" }).done(function (response) {
+                console.log(response.data);
+
+                var results = response.data;
+                // for loop goes through each gif and adds variables
+                for (var i = 0; i < results.length; i++) {
+
+                    var gifDiv = $('<div class=gifs>');
+                    var showGif = $('<img>');
+                    showGif.attr('src', results[i].images.fixed_height_still.url);
+                    // shows rating
+                    showGif.attr('title', "Rating: " + results[i].rating);
+                    showGif.attr('data-still', results[i].images.fixed_height_still.url);
+                    showGif.attr('data-state', 'still');
+                    showGif.addClass('gif');
+                    showGif.attr('data-animate', results[i].images.fixed_height.url);
+
+                    gifDiv.append(showGif)
+
+                    //append gif to html
+                    $("#heroGif").prepend(gifDiv);
+                }
+
+            });
+        }
+
+
+        // function for animating gifs
+        $(document).on('click', '.gif', function () {
+            var state = $(this).attr('data-state');
+            if (state == 'still') {
+                $(this).attr('src', $(this).data('animate'));
+                $(this).attr('data-state', 'animate');
+            } else {
+                $(this).attr('src', $(this).data('still'));
+                $(this).attr('data-state', 'still');
+            };
+        });
+    }
+
+    // function for displaying show gifs
+$(document).on("click", ".show", displayGifs);
+
+// initially calls the makeButtons function
+makeButtons();
